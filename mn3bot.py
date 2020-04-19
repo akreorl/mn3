dccf870f174f8af07b5b1ff9af295dcef5e2f72c
jeongteak <60865072+jeongteak@users.noreply.github.com>
2020-04-19 20:34:35 +0900

    Update mn3bot.py

--- mn3bot.py
+++ mn3bot.py
@@ -23,13 +23,14 @@ async def on_message(message):
     if message.content.startswith("!디코"):
         await message.channel.send("https://discord.gg/rDmbQT2")
     if message.content.startswith("!도움"):
-        await message.channel.send("```접두사는 !입니다 명령어:엠엔,트위치,유튜브,디코,츠키노에용,슈퍼막대기,앱쏠루뜨 입니다 엠엔 잘생김```")
+        await message.channel.send("```접두사는 !입니다 명령어:엠엔,트위치,유튜브,디코,츠키노에용,슈퍼막대기,앱쏠루뜨 입니다 엠엔 잘생김 업데이트는 꼴릴때 합니다 고로 많이 없습니다 ㅋㅋㅋ```")
     if message.content.startswith("!츠키노에용"):
         await message.channel.send("핵쟁이 매니져")
-         if message.content.startswith("!츠키노에용
+    if message.content.startswith("!슈퍼막대기"):
+        await message.channel.send("이거 만든놈")
+    if message.content.startswith("!앱쏠루뜨"):
+        await message.channel.send("저 아닌데요???")
 
         
-         
-
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
